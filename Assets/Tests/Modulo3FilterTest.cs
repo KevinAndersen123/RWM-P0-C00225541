9e268693ecc32d6f4acf8f24abf606f95937fd6b
Kevin Andersen <c00225541@itcarlow.ie>
2020-09-28 12:04:44 +0100

    Ready For testing

---
+++ Assets/Tests/Modulo3FilterTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+//@author Kevin Andersen
+
+namespace Tests
+{
+    public class Modulo3FilterTest
+    {
+        [Test]
+        public void Modulo3FilterSimple()
+        {
+            int[] input = { 7, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = Modulo3Filter.moduloInput(input);
+            int[] expected = { 1, 2, 2, 0, 0, 1, 1, 2, 2, 0 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
